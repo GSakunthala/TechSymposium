f05f4d0774c00d8a88af811bbb542b0b3cd741f9
SakuDivya <sakunthalagnanasekar@gmail.com>
2015-03-24 14:22:20 +0530

    Initial Commit

---
+++ TechSymposium/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  TechSymposium
+//
+//  Created by Kiran Bandla on 3/24/15.
+//  Copyright (c) 2015 MCOE. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
